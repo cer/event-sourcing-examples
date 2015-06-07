d8e26403986fa41c6b7d2ccc3bd6fec559229374
Chris Richardson <chris@chrisrichardson.net>
2015-06-07 15:11:18 -0700

    Implemented hack to eliminate bean @Autowiring circular references

--- java-spring/accounts-query-side-web/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/web/queryside/QuerySideWebConfiguration.java
+++ java-spring/accounts-query-side-web/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/web/queryside/QuerySideWebConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
@@ -21,15 +22,15 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @ComponentScan
 public class QuerySideWebConfiguration extends WebMvcConfigurerAdapter {
 
-	@Autowired
-	private RequestMappingHandlerAdapter adapter;
+	class FakeThing {}
 
-	@PostConstruct
-	public void init() {
+	@Bean
+	public FakeThing init(RequestMappingHandlerAdapter adapter) {
 		// https://jira.spring.io/browse/SPR-13083
 		List<HandlerMethodReturnValueHandler> handlers = new ArrayList<HandlerMethodReturnValueHandler>(adapter.getReturnValueHandlers());
 		handlers.add(0, new ObservableReturnValueHandler());
 		adapter.setReturnValueHandlers(handlers);
+		return new FakeThing();
 	}
 
 }
