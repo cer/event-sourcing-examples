43916e10aa35d8064a66d96ad6b77d3f5298595f
Main <stanislav.semichevskiy@digitalarrowtech.com>
2016-02-17 00:12:38 +0300

    temporarily removed authentication

--- java-spring/monolithic-service/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/web/BankingWebConfiguration.java
+++ java-spring/monolithic-service/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/web/BankingWebConfiguration.java
@@ -33,6 +33,6 @@ public class BankingWebConfiguration extends WebMvcConfigurerAdapter {
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/*").setViewName("forward:/index.html");
+    registry.addViewController("/").setViewName("forward:/index.html");
   }
 }
