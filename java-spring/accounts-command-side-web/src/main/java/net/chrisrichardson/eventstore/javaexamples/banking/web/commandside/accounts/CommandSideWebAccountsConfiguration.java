c5778b1379309525c878757ca331c3c3c4026b75
Chris Richardson <chris@chrisrichardson.net>
2016-03-07 17:52:56 -0800

    Added missing @EnableAutoConfiguration annotations

--- java-spring/accounts-command-side-web/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/web/commandside/accounts/CommandSideWebAccountsConfiguration.java
+++ java-spring/accounts-command-side-web/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/web/commandside/accounts/CommandSideWebAccountsConfiguration.java
@@ -1,25 +1,23 @@
 package net.chrisrichardson.eventstore.javaexamples.banking.web.commandside.accounts;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
 import net.chrisrichardson.eventstore.javaexamples.banking.backend.commandside.accounts.AccountConfiguration;
 import net.chrisrichardson.eventstore.javaexamples.banking.web.util.ObservableReturnValueHandler;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @Import({AccountConfiguration.class})
 @ComponentScan
+@EnableAutoConfiguration
 public class CommandSideWebAccountsConfiguration extends WebMvcConfigurerAdapter {
 
 	class FakeThing {}
